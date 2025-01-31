93c3172aacb234074958b22ef08c0938d81bf0d9
Noran Salah <noransalah840@gmail.com>
2025-01-31 03:18:10 +0200

    Demo

--- Assignment OOP4/Demo.cs
+++ Assignment OOP4/Demo.cs
@@ -8,5 +8,39 @@ namespace Assignment_OOP4
 {
     internal class Demo
     {
+        internal class Rectangle : Shape
+        {
+            public override double Perimeter
+            {
+                get
+                {
+                    return (Dim01 + Dim02) * 2;
+                }
+            }
+
+            public override double GetArea()
+            {
+                return Dim02 * Dim01;
+
+
+            }
+
+
+            public double Dim01 { get; set; }
+            public double Dim02 { get; set; }
+
+            // Abstract Method 
+            public abstract double GetArea();
+
+            // Abstract Property
+            public abstract double Perimeter { get; }
+
+
+            public void Print()
+            {
+                Console.WriteLine("I am Shape");
+            }
+        }
     }
+
 }
