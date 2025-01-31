cb4f32f95e4a18aa8aa4504829c3a55c1bc329d5
Noran Salah <noransalah840@gmail.com>
2025-01-31 03:07:17 +0200

    .

--- Assignment OOP4/Program.cs
+++ Assignment OOP4/Program.cs
@@ -4,8 +4,61 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Question1
+            #region 3.Read from the User the Coordinates for 2 points P1, P2
+            //Console.WriteLine(" enter point P1");
+            //Point3D p1 = Point3D.Create3Dpoint();
+            //Console.WriteLine(" enter point P2");
+            //Point3D p2 = Point3D.Create3Dpoint();
+            //Console.WriteLine(p1);
+            //Console.WriteLine(p2);
 
-            }
+
+
+
+
+            #endregion
+
+            #region 4.Try to use == If(P1 == P2)   Does it work properly? 
+            //if(p1==p2)
+            //	Console.WriteLine("P1==p2");
+            //else
+            //	Console.WriteLine("p1!=p2");
+
+
+            #endregion
+
+            #region 5.Define array of points and sort this array based on X & Y coordinates.
+            //int size;
+            //bool flag;
+            //do
+            //{
+            //	Console.WriteLine("please enter size of array");
+            //	flag =int.TryParse(Console.ReadLine(),out size);
+
+            //} while (!flag);
+            //Point3D[] pointarr=new Point3D[size];
+            //for(int i = 0; i < size; i++)
+            //{
+            //	Console.WriteLine($"Enter cordinate of point number{i+1}");
+            //	pointarr[i] = Point3D.Create3Dpoint();
+            //}
+            //Console.Clear();
+            //Console.WriteLine("Array before sort");
+            //Point3D.Printarr(pointarr);
+            //Console.WriteLine("===============================");
+            //Console.WriteLine("Array after sort");
+            //Point3D.sortarray(pointarr);
+            //Point3D.Printarr(pointarr);
+
+
+
+
+
+            #endregion
+
+
+
+        }
     }
 }
